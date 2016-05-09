dc85c44cf0e956e96c68af004149f2a40fe693e4
Ryan Spangler <ryan.spangler@gmail.com>
2016-05-09 11:23:50 -0700

    rearranging repo for clarity

--- convert/convert-test.sh
+++ convert/convert-test.sh
@@ -4,10 +4,10 @@
 
 #identify the files/filepaths of working directory and converter script
 CWD=$(pwd)
-CONVERTER_SCRIPT=${CWD}/converter_tcga.py
+CONVERTER_SCRIPT=${CWD}/convert-tcga.py
 
 #Convert maf file
-TCGA_PANCAN12_MAF_PATH=${CWD}"/raw_data/pancan12_cleaned_SHORT500.maf"
-TCGA_PANCAN12_CLINICAL_PATH=${CWD}"/raw_data/summary_patient_metadata_pancan12_SHORT100.tsv"
-JSON_OUT_PATH=${CWD}"/converted_data/pancan_patients_binary_3.pbf"
+TCGA_PANCAN12_MAF_PATH=${CWD}"/raw-data/pancan12_cleaned_SHORT500.maf"
+TCGA_PANCAN12_CLINICAL_PATH=${CWD}"/raw-data/summary_patient_metadata_pancan12_SHORT100.tsv"
+JSON_OUT_PATH=${CWD}"/protobuf/pancan_patients_binary_3.pbf"
 python $CONVERTER_SCRIPT --maf $TCGA_PANCAN12_MAF_PATH --tsv $TCGA_PANCAN12_CLINICAL_PATH --outfile $JSON_OUT_PATH
